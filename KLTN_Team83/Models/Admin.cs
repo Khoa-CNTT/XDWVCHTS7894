4e64d42436208913323e4b49e1dfda0578c655eb
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-04-04 16:21:23 +0700

    add-model

---
+++ KLTN_Team83/Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KLTN_Team83.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int id_Admin { get; set; }
+        [Required]
+        public int id_Acc { get; set; }
+        public string adminName { get; set; }
+        public string email { get; set; }
+
+    }
+}
---
+++ KLTN_Team83/Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KLTN_Team83.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int id_Blog { get; set; }
+        [Required]
+        public int id_Expert { get; set; }
+        public string tilte { get; set; }
+        public string content { get; set; }
+        public string img { get; set; }
+        public DateTime ngayTao { get; set; }
+    }
+}

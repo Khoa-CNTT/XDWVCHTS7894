1e1270c62a0ae6ef8576375b7e9f3a06721b0c35
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-04-04 16:19:59 +0700

    update-usermodel

--- KLTN_Team83/Models/User.cs
+++ KLTN_Team83/Models/User.cs
@@ -5,12 +5,14 @@ namespace KLTN_Team83.Models
     public class User
     {
         [Key]
-        public int Id_user { get; set; }
+        public int id_User { get; set; }
 
-        [Required, StringLength(100)]
-        public string NameUser { get; set; }
-
-        [Required, StringLength(255)]
-        public string Password { get; set; }
+        [Required]
+        public int id_Acc { get; set; }
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public float height { get; set; }
+        public float weight { get; set; }
     }
 }

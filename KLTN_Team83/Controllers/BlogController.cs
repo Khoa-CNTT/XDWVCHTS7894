e17d5e6cffe141a1cc8b1783c013090e6c16f28f
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-04-14 10:34:01 +0700

    update-controller

--- KLTN_Team83/Controllers/BlogController.cs
+++ KLTN_Team83/Controllers/BlogController.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KLTN_Team83.Data;
+using KLTN_Team83.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KLTN_Team83.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+        public BlogController(ApplicationDbContext db   )
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Blog> objBlogList = await _db.Blogs.ToListAsync();
+            return View(objBlogList);
+        }
+        public IActionResult CreateBlog()
         {
             return View();
         }
--- KLTN_Team83/Controllers/HomeController.cs
+++ KLTN_Team83/Controllers/HomeController.cs
@@ -22,6 +22,14 @@ namespace KLTN_Team83.Controllers
         {
             return View();
         }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Chat()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

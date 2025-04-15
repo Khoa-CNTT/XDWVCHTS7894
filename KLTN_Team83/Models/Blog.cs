953265ca5b8e8cebd7270fb9bdb142df9ce99974
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-04-15 15:53:27 +0700

    update-modelBlog

--- KLTN_Team83/Models/Blog.cs
+++ KLTN_Team83/Models/Blog.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace KLTN_Team83.Models
 {
@@ -7,9 +8,17 @@ namespace KLTN_Team83.Models
         [Key]
         public int id_Blog { get; set; }
         [Required]
-        public int id_Expert { get; set; }
+
+        [DisplayName("Tilte")]
+        [MaxLength(200, ErrorMessage = "Tiêu đề không được quá 200 ký tự")]
         public string tilte { get; set; }
+
+        [DisplayName("Content")]
+        [MaxLength(3000, ErrorMessage = "Nội dung không được quá 3000 ký tự")]
         public string content { get; set; }
+
+        [DisplayName("Image")]   
+
         public string img { get; set; }
         public DateTime ngayTao { get; set; }
     }

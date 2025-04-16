489a72ba9d778d22ac9ffdd621919a2b4ca37cc9
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-04-16 16:39:01 +0700

    update-N-tier

---
+++ KLTN_Team83.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using KLTN_Team83.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace KLTN_Team83.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext( DbContextOptions <ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        //public DbSet<MyImage> MyImages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //        //base.OnModelCreating(modelBuilder);
+            //        // Configure the primary key for the User entity
+            //        modelBuilder.Entity<User>()
+            //            .HasKey(a => a.id_User);
+            modelBuilder.Entity<Blog>().HasData(
+                new Blog { id_Blog = 1, tilte = "user1", content = "user1@gmail.com", img = "nam.jpg" },
+                new Blog { id_Blog = 2, tilte = "user2", content = "user1@gmail.com", img = "Nam.jpg" },
+                new Blog { id_Blog = 3, tilte = "user3", content = "user1@gmail.com", img = "Healthy.jpg" }
+                );
+            //        // Configure the primary key for the Admin entity
+            //        modelBuilder.Entity<Admin>()
+            //            .HasKey(a => a.id_Admin);
+            //        // Configure the primary key for the Blog entity
+            //        modelBuilder.Entity<Blog>()
+            //            .HasKey(b => b.id_Blog);
+            //        // Configure the primary key for the Expert entity
+            //        modelBuilder.Entity<Expert>()
+            //            .HasKey(e => e.id_Expert);
+            //        // Configure the primary key for the Account entity
+            //        modelBuilder.Entity<Account>()
+            //            .HasKey(a => a.id_Acc);
+        }
+
+    }
+}
---
+++ KLTN_Team83.Models/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KLTN_Team83.Models
+{
+    public class Account
+    {
+        [Key]
+        public int id_Acc { get; set; }
+
+        [Required]
+        public string email { get; set; }
+        public string passWord { get; set; }
+        public string role { get; set; }
+
+    }
+}

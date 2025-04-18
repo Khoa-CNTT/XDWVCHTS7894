141b6157bee185fbe10cba48d7165602bb762e39
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-04-18 15:33:58 +0700

    update-database

--- KLTN_Team83.DataAccess/Data/ApplicationDbContext.cs
+++ KLTN_Team83.DataAccess/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace KLTN_Team83.DataAccess.Data
 
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Account> Accounts { get; set; }
+        public DbSet<InfoUser> Users { get; set; }
         //public DbSet<MyImage> MyImages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -18,10 +19,13 @@ namespace KLTN_Team83.DataAccess.Data
             //        // Configure the primary key for the User entity
             //        modelBuilder.Entity<User>()
             //            .HasKey(a => a.id_User);
-            modelBuilder.Entity<Blog>().HasData(
-                new Blog { id_Blog = 1, tilte = "user1", content = "user1@gmail.com", img = "nam.jpg" },
-                new Blog { id_Blog = 2, tilte = "user2", content = "user1@gmail.com", img = "Nam.jpg" },
-                new Blog { id_Blog = 3, tilte = "user3", content = "user1@gmail.com", img = "Healthy.jpg" }
+            //modelBuilder.Entity<Blog>().HasData(
+            //    new Blog { id_Blog = 1, tilte = "user1", content = "user1@gmail.com", img = "nam.jpg" },
+            //    new Blog { id_Blog = 2, tilte = "user2", content = "user1@gmail.com", img = "Nam.jpg" },
+            //    new Blog { id_Blog = 3, tilte = "user3", content = "user1@gmail.com", img = "Healthy.jpg" }
+            //    );
+            modelBuilder.Entity<InfoUser>().HasData(
+                new InfoUser {id_User=1, id_Account = 1, fullName = "Nguyễn Lê Trung Khánh", age = 22, gender = "Male", height=176, weight=84 }
                 );
             //        // Configure the primary key for the Admin entity
             //        modelBuilder.Entity<Admin>()
@@ -32,8 +36,8 @@ namespace KLTN_Team83.DataAccess.Data
             //        // Configure the primary key for the Expert entity
             //        modelBuilder.Entity<Expert>()
             //            .HasKey(e => e.id_Expert);
-            //        // Configure the primary key for the Account entity
-            //        modelBuilder.Entity<Account>()
+            //        // Configure the primary key for the Blog entity
+            //        modelBuilder.Entity<Blog>()
             //            .HasKey(a => a.id_Acc);
         }
 
---
+++ KLTN_Team83.DataAccess/Migrations/20250418030134_SeedToDatabase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace KLTN_Team83.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "id_Acc",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "id_Acc",
+                keyValue: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "id_Acc", "email", "passWord", "role" },
+                values: new object[,]
+                {
+                    { 1, "user1@gmail.com", "jshafsgh", "user" },
+                    { 2, "user2@gmail.com", "hsfagjs", "user" }
+                });
+        }
+    }
+}

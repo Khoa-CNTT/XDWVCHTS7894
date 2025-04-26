431ca28c5ee848f0c63da778bfa59808fddb4bcc
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-04-26 11:04:51 +0700

    create

---
+++ KLTN_Team83/Areas/Admin/Controllers/TypeBlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KLTN_Team83.Areas.Admin.Controllers
+{
+    public class TypeBlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

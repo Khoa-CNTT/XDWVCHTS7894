46f8cbf48feff62ef8f9c6b6dd6218fa5c3cb5a7
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-04-29 14:36:12 +0700

    add-productmodel

---
+++ KLTN_Team83.Models/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace KLTN_Team83.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id_Product { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public string NameProduct { get; set; }
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        [Required]
+        [Display(Name = "NhaCungCap")]
+        public string NCC { get; set; }
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1,1000)]
+        public double Price { get; set; }
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 1000)]
+        public double? Price50 { get; set; }
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public double? Price100 { get; set; }
+
+        public int Id_Category { get; set; }
+        [ForeignKey("Id_Category")]
+        [ValidateNever]
+        public Category Category { get; set; }
+        [ValidateNever]
+        public string? ImgageUrl { get; set; }   
+    }
+}

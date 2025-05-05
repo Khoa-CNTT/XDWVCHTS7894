988507055de85131e0492f2e122a9141fb74aa0d
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-05-05 09:14:21 +0700

    update-view

--- KLTN_Team83/wwwroot/js/product.js
+++ KLTN_Team83/wwwroot/js/product.js
@@ -1,16 +1,47 @@
-﻿$(document).ready(function () {
+﻿
+
+$(document).ready(function () {
     loadDataTable();
 });
 
+//function loadDataTable() {
+//    dataTable = $('#tblData').DataTable({
+//        "ajax": { url:'/admin/product/getall' },
+//        "columns": [
+//            { data: 'nameProduct', "width": "10%" },
+//            { data: 'description', "width": "10%" },
+//            { data: 'ncc', "width": "10%" },
+//            { data: 'price', "width": "10%" },
+//            { data: 'category.name', "width": "10%" }
+//        ]
+//    });
+//}
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url:'admin/product/getall' },
+        "ajax": {
+            url: '/admin/product/getall'
+        },
         "columns": [
-            { data: 'name', "width": "15%" },
-            { data: 'position', "width": "15%" },
-            { data: 'salary', "width": "15%" },
-            { data: 'office', "width": "15%" }
+            { data: 'nameProduct', "width": "20%" },
+            { data: 'description', "width": "30%" },
+            { data: 'ncc', "width": "10%" },
+            { data: 'price', "width": "8%" },
+            { data: 'category.name', "width": "12%" },
+            {
+                data: 'id_Product',
+                render: function (data) {
+                    return `
+                        <div class="text-center w-75 btn-group" role="group">
+                            <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-success text-white mx-2" style="cursor:pointer">
+                                <i class="bi bi-pencil-square"></i>Edit
+                            </a>
+                            <a onclick=Delete('/Admin/Product/Delete/${data}') class="btn btn-danger text-white mx-2" style="cursor:pointer">
+                                <i class="bi bi-trash-fill"></i>Delete
+                            </a>
+                        </div>
+                    `;
+                }, "width": "20%"
+            }
         ]
     });
-}
-
+};

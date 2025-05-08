a144dff960fb4f4a3daf1fbaadd3e63e24033cb0
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-05-08 10:41:34 +0700

    update-viewlayout

---
+++ KLTN_Team83/wwwroot/js/company.js
@@ -0,0 +1,63 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            url: '/admin/company/getall'
+        },
+        "columns": [
+            { data: 'name', "width": "25%" },
+            { data: 'address', "width": "30%" },
+            { data: 'phoneNumber', "width": "20%" },
+            {
+                data: 'id_Company',
+                render: function (data) {
+                    return `
+                        <div class=" w-75 btn-group" role="group">
+                            <a href="/Admin/Company/Upsert?id=${data}" class="btn btn-success text-white mx-2" style="cursor:pointer">
+                                <i class="bi bi-pencil-square"></i>Edit
+                            </a>
+                            <a onclick=Delete('/Admin/Company/Delete/${data}') class="btn btn-danger text-white mx-2" style="cursor:pointer">
+                                <i class="bi bi-trash-fill"></i>Delete
+                            </a>
+                        </div>
+                    `;
+                }, "width": "25%"
+            }
+        ]
+    });
+};
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        toastr.success(data.message);
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                },
+                //title: "Deleted!",
+                //text: "Your file has been deleted.",
+                //icon: "success"
+            });
+        }
+    });
+};
---
+++ KLTN_Team83/wwwroot/js/user.js
@@ -0,0 +1,65 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            url: '/admin/user/getall'
+        },
+        "columns": [
+            { data: "name", "width": "20%" },
+            { data: "email", "width": "20%" },
+            { data: "phoneNumber", "width": "15%" },
+            { data: "company.name", "width": "15%" },
+            { data: "role", "width": "10%" },
+            {
+                data: 'id_Product',
+                render: function (data) {
+                    return `
+                        <div class=" w-75 btn-group text-center" role="group">
+                            <a href="/Admin/User/Upsert?id=${data}" class="btn btn-success text-white mx-2" style="cursor:pointer">
+                                <i class="bi bi-pencil-square"></i>Edit
+                            </a>
+                            <a onclick=Delete('/Admin/User/Delete/${data}') class="btn btn-danger text-white mx-2" style="cursor:pointer">
+                                <i class="bi bi-trash-fill"></i>Delete
+                            </a>
+                        </div>
+                    `;
+                }, "width": "20%"
+            }
+        ]
+    });
+};
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        toastr.success(data.message);
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                },
+                //title: "Deleted!",
+                //text: "Your file has been deleted.",
+                //icon: "success"
+            });
+        }
+    });
+};

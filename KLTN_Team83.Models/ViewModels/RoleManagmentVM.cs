2c889122a683ab48b0bd6f8eeac34ab630edce86
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-05-09 09:23:31 +0700

    add-RoleManagment

---
+++ KLTN_Team83.Models/ViewModels/RoleManagmentVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace KLTN_Team83.Models.ViewModels
+{
+    public class RoleManagmentVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+        public IQueryable<SelectListItem> CompanyList { get; set; }
+    }
+}

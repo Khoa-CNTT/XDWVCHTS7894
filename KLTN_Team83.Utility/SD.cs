7ac6ab487cc3385fdb1e1ba7eaa45ead61828a2e
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-05-13 21:20:11 +0700

    update-Utility

--- KLTN_Team83.Utility/SD.cs
+++ KLTN_Team83.Utility/SD.cs
@@ -8,10 +8,30 @@ namespace KLTN_Team83.Utility
 {
     public static class SD
     {
+        //Role
         public const string Role_Customer = "Customer";
         public const string Role_Expert = "Expert";
         public const string Role_Admin = "Admin";
         public const string Role_Company = "Company";
-        
+
+
+        //Status
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "Processing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+        public const string StatusCompleted = "Completed";
+        public const string StatusDelayed = "Delayed";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
+
+
+        public const string SessionCart = "SessionShoppingCart";
+
     }
 }

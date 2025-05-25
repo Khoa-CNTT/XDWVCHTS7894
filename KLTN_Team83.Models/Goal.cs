74068d07c31669b4a5ee6d77409c218b11c3ad18
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-05-25 11:11:07 +0700

    update-GoalModel

--- KLTN_Team83.Models/Goal.cs
+++ KLTN_Team83.Models/Goal.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace KLTN_Team83.Models
 {
-    public enum GoalType { Hydration, Sleep, Weight, Steps }
+    //public enum GoalType { Hydration, Sleep, Weight, Steps }
     public class Goal
     {
         [Key]
         public int Id_Goal { get; set; }
         [Required]
         public string UserId { get; set; }
-        public virtual ApplicationUser ApplicationUser { get; set; }
-        public GoalType Type { get; set; }
+        [ForeignKey("UserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+        public int Id_GoalType { get; set; }
+        [ForeignKey("Id_GoalType")]
+        [ValidateNever]
+        public GoalType GoalType { get; set; }
         public double TargetValue { get; set; }
         public DateTime TargetDate { get; set; }
     }
